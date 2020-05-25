7bdcc96c83b8a09f607f185b66932a72c19f5b47
nagolove <dekar.own@gmail.com>
2020-05-25 19:45:03 +0300

    Trying create Hex class.

--- conf.lua
+++ conf.lua
@@ -1,6 +1,7 @@
 local inspect = require "inspect"
 function love.conf(t)
     --print("t", inspect(t))
+    t.console = true
     t.window.fullscreen = true
     t.window.vsync = false
 end
--- main.lua
+++ main.lua
@@ -7,7 +7,23 @@ function dist(x1, y1, x2, y2)
 end
 
 function genHexPolygon(cx, cy, rad)
-    local hex = {}
+    local Hex = {}
+    Hex.__index = Hex
+
+    function Hex:getVertices()
+        local vertices = {}
+        for i = 1, 6 do
+            table.insert(vertices, self[i])
+        end
+        return vertices
+    end
+
+    function Hex:setMesh(mesh, startindex)
+        self.mesh = mesh
+        self.startindex = startindex
+    end
+
+    local hex = setmetatable({}, Hex)
 
     local d = math.pi * 2 / 6
     local c = 0
@@ -33,29 +49,71 @@ function getHexPolygonHeight(hex)
 end
 
 function genHexField(startcx, startcy, xcount, ycount, rad)
+
+    function addVertex(array, x, y)
+        table.insert(array, {
+            x, y,
+            0, 0, -- u v
+            0.5, 0.5, 1, 1
+        })
+    end
+
     local result = {}
-    local mesh = gr.newMesh(7 * xcount * ycount, "fan", "dynamic")
+    local mesh = gr.newMesh(18 * xcount * ycount, "triangles", "dynamic")
+    local meshData = {}
+
     local cx, cy = startcx, startcy
     local hasWH = false
     local w, h
     for j = 1, ycount do
-    for i = 1, xcount do
-        table.insert(result, genHexPolygon(cx, cy, rad))
-        if not hasWH then
-            w, h = getHexPolygonWidth(result[#result]), 
-                getHexPolygonHeight(result[#result])
+        for i = 1, xcount do
+            local last = genHexPolygon(cx, cy, rad)
+            table.insert(result, last)
+            if not hasWH then
+                w, h = getHexPolygonWidth(result[#result]), 
+                    getHexPolygonHeight(result[#result])
+            end
+
+            local lastHex = last
+
+            addVertex(meshData, lastHex.cx, lastHex.cy)
+            addVertex(meshData, lastHex[1], lastHex[2])
+            addVertex(meshData, lastHex[3], lastHex[4])
+
+            addVertex(meshData, lastHex.cx, lastHex.cy)
+            addVertex(meshData, lastHex[3], lastHex[4])
+            addVertex(meshData, lastHex[5], lastHex[6])
+
+            addVertex(meshData, lastHex.cx, lastHex.cy)
+            addVertex(meshData, lastHex[5], lastHex[6])
+            addVertex(meshData, lastHex[7], lastHex[8])
+
+            addVertex(meshData, lastHex.cx, lastHex.cy)
+            addVertex(meshData, lastHex[7], lastHex[8])
+            addVertex(meshData, lastHex[9], lastHex[10])
+
+            addVertex(meshData, lastHex.cx, lastHex.cy)
+            addVertex(meshData, lastHex[9], lastHex[10])
+            addVertex(meshData, lastHex[11], lastHex[12])
+
+            addVertex(meshData, lastHex.cx, lastHex.cy)
+            addVertex(meshData, lastHex[11], lastHex[12])
+            addVertex(meshData, lastHex[1], lastHex[2])
+
+            cx = cx + w
         end
-        cx = cx + w
-    end
         cy = cy + h * 3 / 4
         cx = j % 2 == 1 and startcx + w / 2 or startcx
     end
-    return result
+
+    mesh:setVertices(meshData)
+
+    return result, mesh
 end
 
 local hex = genHexPolygon(100, 100, 50)
 local hexRad = 50
-local hexField = genHexField(80, 80, 20, 20, hexRad)
+local hexField, hexMesh = genHexField(80, 80, 200, 200, hexRad)
 local hexHeight = getHexPolygonHeight(hex)
 local hexWidth = getHexPolygonWidth(hex)
 
@@ -94,7 +152,12 @@ end
 
 love.draw = function()
     gr.setColor{0, 0.85, 0.1, 0.5}
-    drawHexField(hexField)
+    --drawHexField(hexField)
+
+    gr.setColor{1, 1, 1, 1}
+    gr.draw(hexMesh)
+    
+    --gr.draw(testMesh)
     gr.setColor{1, 1, 1, 1}
     gr.print(string.format("FPS %d", love.timer.getFPS()))
 end
